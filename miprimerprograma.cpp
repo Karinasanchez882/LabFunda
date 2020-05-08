0f0a2bcbf7d5d5bbb8e08f84a605c89fc2d6cab2
karinasanchez882 <00081920@uca.edu.sv>
2020-05-08 18:58:22 +0200

    misegundocommit

---
+++ miprimerprograma.cpp
@@ -0,0 +1,14 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+cout << " hola soy tu primer programa." << endl;
+cout << " mucho gusto en conocerte.";
+
+ cout << endl;
+return 0;
+
+
+}
---
+++ misegundoprograma.cpp
@@ -0,0 +1,11 @@
+#include "iostream"
+
+using namespace std;
+ int main()
+
+ {
+      cout << "esta es una frase"  << endl;
+
+return 0;
+
+ }
